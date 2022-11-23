917e08d64bac7db4adec313df8dac15d3f96f398
Luiz Vidal <luizfernandovidalsilva1@gmail.com>
2022-11-22 23:18:35 -0300

    :art: conditionals

---
+++ tutorials/1_complete-beginners/001_javascript-basics.js
@@ -0,0 +1,25 @@
+// comments https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/JavaScript_basics#comments
+// everything after two / is a comment, and everything between /*'coment'*/ is a comment
+
+// variables
+/*let variableName; //variable declared
+variableName = 10; // variableName recieves 10;
+console.log(variableName) // 10 */
+
+
+// operators https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/JavaScript_basics#operators
+/*+ sum two values or concat then if they are strings 
+- subtract two values
+= assign a value to a variable
+* multiply two values
+% return the rest of a division between two numbers, ex: 5 % 2 = 1*/
+
+//conditionals https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/JavaScript_basics#conditionals
+
+let iceCream = 'chocolate';
+if (iceCream === 'chocolate') {
+    console.log('Yes, I love chocolate ice cream')
+} else {
+    console.log('Chocolate is my favorite, but i\'ll accept')
+}
+

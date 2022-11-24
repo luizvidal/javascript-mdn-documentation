5ac3be1090db0dde703053b29da06f7c40d594af
Luiz Vidal <luizfernandovidalsilva1@gmail.com>
2022-11-23 22:01:14 -0300

    :zap: javascript guide

---
+++ tutorials/2_javascript-guide/001_javascript-guide.js
@@ -0,0 +1,8 @@
+// hello world https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Introduction#hello_world
+
+/*function greetMe(yourName) {
+    console.log(`Hello ${yourName}!`);
+};
+
+greetMe('World')*/
+// Variable scope https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Grammar_and_types#variable_scope

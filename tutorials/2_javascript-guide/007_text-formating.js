7fc9d422caad4a20ff525078a3613ac5c2d28a1f
Luiz Vidal <luizfernandovidalsilva1@gmail.com>
2022-11-26 16:09:39 -0300

    🚀 javascript - text formating

---
+++ tutorials/2_javascript-guide/007_text-formating.js
@@ -0,0 +1,65 @@
+// console.log('\u00A9');
+
+// const foo = 'foo';
+// console.log(typeof foo); // string
+// const foo2 = new String('foo');
+// console.log(typeof foo2) // object
+
+// const firstString = '2 + 2';
+// const secondString = new String('2 + 2');
+// console.log(eval(firstString));
+// console.log(eval(secondString));
+
+// const hello = 'Hello, World!';
+// const helloLength = hello.length // 13;
+// hello[0]; // H
+
+const string = "Some random string...";
+const anotherString = " Another string!";
+const string2 = " A ";
+// console.log(string.charAt(3)) //e
+// console.log(string.indexOf('dom'))//8
+// console.log(string.startsWith('Some ran'))//true;
+// console.log(string.endsWith('ng..'))//false
+// console.log(string.includes('random'))//true
+// console.log(string.concat(anotherString))
+// console.log(string.split())// ['Some random string...']
+// const slice = string.slice(-9) //string...
+// console.log(slice)
+
+// console.log(string.substring(4,11))//random
+
+// console.log(string.toLowerCase())//some random sring...
+// console.log(anotherString.toUpperCase())// ANOTHER STRING...
+
+// console.log(string2.repeat(3))//' A  A A '
+// console.log(string2.trim())//'A'
+
+// console.log(`Multi lines
+// with template
+// literals`)
+
+// Date and time formating
+// const july172014 = new Date("2014-07-17");
+// const options = {
+//   year: "2-digit",
+//   month: "2-digit",
+//   day: "2-digit",
+//   hour: "2-digit",
+//   minute: "2-digit",
+//   timeZoneName: "short",
+// };
+// const americanDateTime = new Intl.DateTimeFormat('en-Us', options).format
+
+// console.log(americanDateTime(july172014))
+
+// Number formatting
+
+// const gasPrice = new Intl.NumberFormat("en-US", {
+//   style: "currency",
+//   currency: "USD",
+//   minimumFractionDigits: 3
+// });
+
+// console.log(gasPrice.format(5.259))//$5.259
+

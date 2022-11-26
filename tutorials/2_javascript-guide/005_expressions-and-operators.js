e29acd8f5ff01ae29c98b8b2182ca789478fb550
Luiz Vidal <luizfernandovidalsilva1@gmail.com>
2022-11-26 11:53:02 -0300

    ✨ javascript expressions and operators

---
+++ tutorials/2_javascript-guide/005_expressions-and-operators.js
@@ -0,0 +1,112 @@
+// const obj = {};
+
+// obj.x = 3;
+
+// console.log(obj.x);
+// console.log(obj);
+
+// const key = "y";
+// obj[key] = 5;
+// console.log(obj[key]);
+// console.log(obj);
+
+// const foo = ['one', 'two', 'three'];
+
+// const one = foo[0];
+// const two = foo[1];
+// const three = foo[2];
+
+// console.log(one, two, three)
+
+// const [a, b ,c] = foo;
+
+// console.log(a, b, c)
+
+// function f() {
+//     console.log('F');
+//     return 2;
+// }
+
+// function g() {
+//     console.log('G');
+//     return 3;
+// }
+
+// let x, y
+
+// y = x = f()
+// y = [ f(), x = g() ]
+// x[f()] = g()
+// console.log(+true)
+
+// Bigints operators
+
+// const a = Number(1n) + 2;
+// console.log(a);
+
+// Stirng operators
+
+// console.log('My', 'string');
+// let myStirng = 'alpha';
+// myStirng += 'bet';
+
+// console.log(myStirng);
+
+// ternary operator;
+
+// let age = 19;
+// const status  = (age >= 18) ? 'Major' : 'Minor';
+// console.log(status);
+
+// const x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const a = [x, x, x, x, x,];
+
+// for (let i = 0, j = 9; i <= j; i++, j--) {
+//     console.log(`a[${i}][${j}] = ${a[i][j]}`)
+// };
+
+// console.log(a)
+
+// delete
+// let obj = { a: 1, b: 2, c: 2, d: 3 };
+// delete obj.b;
+// delete obj['c'];
+
+// const myFun = new Function();
+// const shape = 'round';
+// const size = 1;
+// const foo = ['Apple', 'Mango', 'Orange'];
+// const today = new Date();
+
+// console.log(typeof null);
+
+// In 
+
+// const trees = ['redwood', 'bay', 'cedar', 'oak', 'maple'];
+// const myCar = { a: 1, b:2 };
+// console.log('a' in myCar);
+
+// Instaceof 
+
+// objectName instanceof objectType
+// const arr = [1, 2, 3, 4];
+// if (arr instanceof Array) console.log(arr)
+// const theDay = new Date(2002, 1, 1);
+// if (theDay instanceof Date) {
+//     console.log(theDay);
+// }
+// this
+
+// this['propertyName'];
+// this.propertyName;
+
+
+// function validate(obj, lowval, hival) {
+//     if (obj.value < lowval || (obj.value > hival)) {
+//         console.log('Invalid Value!');
+//     }
+// }
+
+
+
+
